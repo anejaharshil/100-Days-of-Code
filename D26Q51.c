b1d119e291e68841adbbde780b5eb5ac77bce668
anejaharshil <anejaharshil@gmail.com>
2026-09-04 11:59:24 +0530

    COMPLETED CODES

---
+++ D26Q51.c
@@ -0,0 +1,40 @@
+/* Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+#include <Stdio.h>
+int main(){
+    int n , i , j ;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for ( i = n ; i >= 1 ; i-- ){
+
+        for ( j = 1 ; j < i ; j++ ){
+            printf(" ");
+        }
+
+        for ( j = i ; j <= n ; j++ ){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
